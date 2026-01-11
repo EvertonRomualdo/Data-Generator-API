d73dc27b9946d212188dee8466fa20242c389fcd
Everton Daniel <evertonrdaniel@gmail.com>
2026-01-10 23:18:58 -0300

    feat: initial setup

---
+++ src/main/java/br/com/everton/datageneratorapi/DatageneratorapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.everton.datageneratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatageneratorapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatageneratorapiApplication.class, args);
+	}
+
+}
